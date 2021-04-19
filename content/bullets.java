35bac6ef03026cf2579da4c612200f64281c82b8
DireWolf575 <79067788+DireWolf575@users.noreply.github.com>
2021-04-19 18:13:10 -0400

    Update and rename bullets.hjson to bullets.java

---
+++ content/bullets.java
@@ -0,0 +1,9 @@
+scrapFrag = new BasicBulletType(3f, 5, "bullet"){{
+            width = 5f;
+            height = 12f;
+            shrinkY = 1f;
+            lifetime = 20f;
+            backColor = Color.gray;
+            frontColor = B8860B;
+            despawnEffect = Fx.none;
+        }};
